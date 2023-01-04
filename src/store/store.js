eea20b8555175d609af9e1645d39b34337e9b9b1
Artem Kalach <rtmklcwork@gmail.com>
2023-01-04 09:01:41 +0300

    finaly

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,42 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER
+} from 'redux-persist';
+import storage from "redux-persist/lib/storage";
 import todoSlice from "../features/todo/todoSlice";
 import userSlice from "../features/user/userSlice";
 
+const rootReducer = combineReducers({
+    user: userSlice,
+    todo: todoSlice,
+})
 
- const store = configureStore({
-    reducer: {
-        user: userSlice,
-        todo: todoSlice,
-       
-    },
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+
+
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
+export const persistor = persistStore(store);
 export default store;
